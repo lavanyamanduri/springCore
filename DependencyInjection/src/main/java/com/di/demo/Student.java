258fe90d0963d5e42ec2bbfa071bafd7553e331f
lavanyamanduri <lavanya.manduri@gmail.com>
2020-08-03 15:36:06 +0530

    [lavanya] DependencyInjection Example

---
+++ DependencyInjection/src/main/java/com/di/demo/Student.java
@@ -0,0 +1,17 @@
+package com.di.demo;
+
+public class Student {
+
+	private String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void display()
+	{
+		System.out.print(name);
+		
+	}
+}
+
