98767229f3db2ac1a55d299b4c94998a74f9c04c
lavanyamanduri <lavanya.manduri@gmail.com>
2020-08-03 15:37:19 +0530

    [lavanya] xmlResponse Example

---
+++ XmlResponse/src/main/java/com/xml/demo/XmlResponseApplication.java
@@ -0,0 +1,13 @@
+package com.xml.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlResponseApplication.class, args);
+	}
+
+}
---
+++ XmlResponse/src/test/java/com/xml/demo/XmlResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xml.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
